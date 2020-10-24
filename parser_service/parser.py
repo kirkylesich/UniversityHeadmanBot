74920bf7433d3cc49743da7c61855c090b5ea99c
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2020-10-24 20:26:13 +0500

    Преобразование ексель таблиц

--- parser_service/parser.py
+++ parser_service/parser.py
@@ -6,10 +6,60 @@ from utils import Timetable
 from typing import Dict
 from bs4 import BeautifulSoup
 
+import pandas as pd
+from utils import TimetableObject
+from utils import StudyDay
+from utils import Timetable
 
 class TimeTableExcelParser:
     def parse_to_json(self):
         pass
+
+    def convert_to_dataclass(path):
+        table = pd.ExcelFile(path)
+        df1 = table.parse(table.sheet_names)
+        inp = df1.get('Raspisanie').values.tolist()
+        f_st = inp[0][0]
+        w = f_st[14:16]
+        if w[1]==' ':
+          num = int (w[0])
+        else :
+          num = int (w[0])*10 + int (w[1])
+
+        time_start=[]
+        time_finish=[]
+
+        for i in range(0, 1):
+          for st in range(3,10):
+            ll = inp[st][i][7:]
+            index = int (str.find(ll,' ',))
+            time_start.append(ll[1:index])
+            time_finish.append(ll[index+3:])
+        weeks=[]
+        for day in range(1, 7):
+          name=[]
+          class_order=[]
+          for line in range(3,10):
+            if inp[line][day]!='nan':
+              stringf = clear_stringf(str (inp[line][day]))
+              name.append(stringf[1:])
+          weeks.append(name)
+        week_info = []
+        for i in range(0, 6):
+          day_info = []
+          for el in range(0, 7):
+            element = TimetableObject(time_start=time_start[el], time_finish=time_finish[el], name=weeks[i][el], class_order=el)
+            day_info.append(element)
+          k = StudyDay(day_info)
+          week_info.append(k)
+        Upper = Timetable( week=num, timatable_days= week_info)
+        pass
+
+    def clear_stringf(s):
+      for i in range(0,len(s)):
+        if s[i]=='\n':
+          s = s[0:i] + ' ' + s[i+1:];
+      return s
 
 
 class SheduleType(Enum):
